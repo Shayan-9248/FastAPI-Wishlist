5c2a37ef37a4af9894a61c9d65406ff709781c86
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-07-25 22:47:29 +0430

    feat: implement get-user api

--- app/users/api/v1.py
+++ app/users/api/v1.py
@@ -15,3 +15,13 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(database.g
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return await crud.create_user(db=db, user=user)
+
+
+@router.get("/{user_id}", response_model=schemas.User)
+async def read_user(user_id: int, db: Session = Depends(database.get_db)):
+    db_user = await crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(
+            status_code=404, detail=f"User with id {user_id} does not exists"
+        )
+    return db_user
