282f13b42633c017657115ca36858597e324bd27
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-07-26 22:40:00 +0430

    feat: Implement delete-comment api

--- app/comments/api/v1.py
+++ app/comments/api/v1.py
@@ -29,6 +29,13 @@ async def read_product_comments(
 
 @router.put("/update/{comment_id}")
 async def update_comment(
-    comment_id: int, comment: schemas.CommentUpdate, db: Session = Depends(database.get_db)
+    comment_id: int,
+    comment: schemas.CommentUpdate,
+    db: Session = Depends(database.get_db),
 ):
     return await crud.update_comment(db=db, comment_id=comment_id, comment=comment)
+
+
+@router.delete("/delete/{comment_id}")
+async def delete_comment(comment_id: int, db: Session = Depends(database.get_db)):
+    return await crud.delete_comment(db=db, comment_id=comment_id)
--- app/comments/crud.py
+++ app/comments/crud.py
@@ -27,17 +27,28 @@ async def get_product_comments(db: Session, product_id: int):
 
 
 async def update_comment(comment_id: int, db: Session, comment: schemas.CommentUpdate):
-    db_folder = db.query(models.Comment).filter(
-        models.Comment.id == comment_id
-    )
-    if not db_folder.first():
+    db_comment = db.query(models.Comment).filter(models.Comment.id == comment_id)
+    if not db_comment.first():
         raise HTTPException(
             status_code=404, detail=f"Comment with id {comment_id} does not exists."
         )
-    db_folder.update(
+    db_comment.update(
         {
             models.Comment.content: comment.content,
         }
     )
+    db.commit()
+    return "Done"
+
+
+async def delete_comment(comment_id: int, db: Session):
+    db_comment = db.query(models.Comment).filter(models.Comment.id == comment_id)
+
+    if not db_comment.first():
+        raise HTTPException(
+            status_code=404, detail=f"Comment with id {comment_id} does not exists."
+        )
+
+    db_comment.delete()
     db.commit()
     return "Done"
